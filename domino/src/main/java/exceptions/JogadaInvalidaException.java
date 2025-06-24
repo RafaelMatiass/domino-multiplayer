1d0f57b1552cdb8d732fe619ca3564bba74041da
Rafael Matias <rafaeldavi8086@gmail.comgit config --global user.email rafaeldavi8086@gmail.comexitgit config --lits>
2025-06-24 16:13:56 -0300

    Implementation of specific exceptions: package exceptions

---
+++ domino/src/main/java/exceptions/JogadaInvalidaException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class JogadaInvalidaException extends Exception {
+    public JogadaInvalidaException(String message) {
+        super(message);
+    }
+}
+
---
+++ domino/src/main/java/exceptions/PoteVazioException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class PoteVazioException extends Exception {
+    public PoteVazioException(String message) {
+        super(message);
+    }
+}
