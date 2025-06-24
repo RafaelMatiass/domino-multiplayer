87e75855831ad6ac7fe96ff45ae761e5013cda83
Rafael Matias <rafaeldavi8086@gmail.comgit config --global user.email rafaeldavi8086@gmail.comexitgit config --lits>
2025-06-24 16:19:16 -0300

    Change: add the MesaDeJogo, starting hands for players, and references for Gerenciador.

--- domino/src/main/java/conexao/ServidorDomino.java
+++ domino/src/main/java/conexao/ServidorDomino.java
@@ -2,12 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package conexao;
 
 /**
  *
  * @author rafaelmatias
  */
+
+package conexao;
 
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -16,6 +17,8 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import jogo.Config;
 import jogo.GerenciadorDeJogadas;
+import jogo.MesaDeJogo; 
+import jogo.Pedra; 
 
 public class ServidorDomino {
     public static void main(String[] args) throws Exception {
@@ -23,13 +26,20 @@ public class ServidorDomino {
         ServerSocket servidor = new ServerSocket(Config.getPorta(), 2, InetAddress.getByName(Config.getIp()));
         System.out.println("Servidor Dominó Inicializado (" + servidor + ").\n");
         
+        MesaDeJogo mesaDeJogo = new MesaDeJogo(); 
+        
         System.out.println("Esperando por conexão (Jogador 1)...");
         Socket jogador1 = servidor.accept();
         System.out.println("Conexão recebida: " + jogador1.toString() + ":" + jogador1.getPort() + "\n");
         
         ObjectOutputStream outJogador1 = new ObjectOutputStream(jogador1.getOutputStream());
         outJogador1.flush();
-        outJogador1.writeObject("J1;true");
+        
+        StringBuilder maoJ1 = new StringBuilder("J1;true");
+        for (Pedra p : mesaDeJogo.getMaoDoJogador("J1")) {
+            maoJ1.append(";").append(p.getLadoA()).append("-").append(p.getLadoB());
+        }
+        outJogador1.writeObject(maoJ1.toString());
         
         ObjectInputStream inJogador1 = new ObjectInputStream(jogador1.getInputStream());
         
@@ -39,12 +49,17 @@ public class ServidorDomino {
         
         ObjectOutputStream outJogador2 = new ObjectOutputStream(jogador2.getOutputStream());
         outJogador2.flush();
-        outJogador2.writeObject("J2;false");
+        
+        StringBuilder maoJ2 = new StringBuilder("J2;false");
+        for (Pedra p : mesaDeJogo.getMaoDoJogador("J2")) {
+            maoJ2.append(";").append(p.getLadoA()).append("-").append(p.getLadoB());
+        }
+        outJogador2.writeObject(maoJ2.toString());
         
         ObjectInputStream inJogador2 = new ObjectInputStream(jogador2.getInputStream());
-        
-        Thread threadJogador1 = new Thread(new GerenciadorDeJogadas(inJogador1, outJogador2));
-        Thread threadJogador2 = new Thread(new GerenciadorDeJogadas(inJogador2, outJogador1));
+ 
+        Thread threadJogador1 = new Thread(new GerenciadorDeJogadas("J1", inJogador1, outJogador2, mesaDeJogo, outJogador1));
+        Thread threadJogador2 = new Thread(new GerenciadorDeJogadas("J2", inJogador2, outJogador1, mesaDeJogo, outJogador2));
         
         threadJogador1.start();
         threadJogador2.start();
